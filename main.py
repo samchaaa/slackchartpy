f701b988d148d0747eb7fee9372ed4a0173bac2b
samchaaa <samchakerian@gmail.com>
2023-03-24 11:14:58 -0700

    color scheme and day breaks

--- main.py
+++ main.py
@@ -17,6 +17,31 @@ client = slack.WebClient(token=SLACK_TOKEN)
 
 import flask
 
+# yahoo
+style = {'base_mpl_style': 'fast',
+         'marketcolors'  : {'candle': {'up': '#579FEC', 'down': '#E28E60'},
+                            'edge'  : {'up': '#579FEC', 'down': '#E28E60'},
+                            'wick'  : {'up': '#579FEC', 'down': '#E28E60'},
+                            'ohlc'  : {'up': '#579FEC', 'down': '#E28E60'},
+                            'volume': {'up': '#579FEC', 'down': '#E28E60'},
+                            'vcedge': {'up': '#579FEC', 'down': '#E28E60'},
+         'vcdopcod'      : True,
+         'alpha'         : 0.9},
+         'mavcolors'     : None,
+         'facecolor'     : '#fafafa',
+         'gridcolor'     : '#d0d0d0',
+         'gridstyle'     : '-',
+         'y_on_right'    : False,
+         'rc'            : {'axes.labelcolor': '#101010',
+                            'axes.edgecolor' : 'f0f0f0',
+                            'axes.grid.axis' : 'y',
+                            'ytick.color'    : '#101010',
+                            'xtick.color'    : '#101010',
+                            'figure.titlesize': 'x-large',
+                            'figure.titleweight':'semibold',
+                           },
+         'base_mpf_style': 'yahoo'}
+
 app = flask.Flask(__name__)
 @app.route('/', methods=['GET'])
 def home():
@@ -24,7 +49,7 @@ def home():
     code = flask.request.args['code']
 
     # req data
-    start, end = int(time.time()), int(time.time() - 60 * 1440) # -1d
+    start, end = int(time.time()), int(time.time() - 60 * 1440 * 1) # -1d
     url = f"https://au.advfn.com/common/javascript/tradingview/advfn/history?symbol=ASX%5E{code}&resolution=5&v=1&from={start}&to={end}"
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     r = requests.get(url, headers=headers)
@@ -46,7 +71,7 @@ def home():
     mpl.plot(
         d,
         type="candle",
-        style="binance",
+        style=style,
         savefig='chart.png',
         xrotation=30,
         fontscale=0.8,
@@ -59,6 +84,10 @@ def home():
             'bottom': 0.75,
             'right': 0.25
         },
+        vlines=dict(
+            vlines=[_ for _ in d.index if _.time().hour == 15 and _.time().minute == 55],
+            alpha=0.5, colors='grey', linestyle='dashed'
+        ),
     )
 
     img = open('chart.png'.format(code), 'rb').read()
